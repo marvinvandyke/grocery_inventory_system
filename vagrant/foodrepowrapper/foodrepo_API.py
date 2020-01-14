de5b14032a3a43f43de7d319501e4943850f0ab8
marvinvandyke <dsagnotti@googlemail.com>
2020-01-14 22:10:56 +0100

    Possibility to search for upcs via open food repo

---
+++ vagrant/foodrepowrapper/foodrepo_API.py
@@ -0,0 +1,46 @@
+import requests as rq
+import json
+
+BASE_URL='https://www.foodrepo.org/api/v3'
+API_KEY='b8e6b727060741a4a0b92fb45e874338'
+
+def getProductByUPC(product_upc):
+
+    ENDPOINT='/products/_search/'
+
+    url = BASE_URL + ENDPOINT
+
+    query = {
+    "query": {
+        "terms" : {
+        "barcode" : [
+            "%s" % product_upc
+                    ]
+                }
+            }
+    }
+
+    headers = {
+    'Authorization': "Token token=" + API_KEY,
+    'Accept': 'application/json',
+    'Content-Type': 'application/vnd.api+json',
+    'Accept-Encoding': 'gzip,deflate'
+    }
+
+    r = rq.post(url, json=query, headers=headers)
+    if r.status_code == 200:
+        if len(r.json()['hits']['hits']) == 1:
+            productInfo = {
+                "id" : r.json()['hits']['hits'][0]['_source']['id'],
+                "upc" : r.json()['hits']['hits'][0]['_source']['barcode'],
+                "name" : r.json()['hits']['hits'][0]['_source']['display_name_translations']['de'],
+                "quantity": r.json()['hits']['hits'][0]['_source']['quantity'],
+                "unit" : r.json()['hits']['hits'][0]['_source']['unit']
+            }
+
+            print(productInfo)
+
+            return productInfo
+
+    return
+
--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
-from vagrant.database_setup.database_setup import Base, StockItem
+from database_setup.database_setup import Base, StockItem
+import foodrepowrapper.foodrepo_API as foodAPI
 
 # Create flask application
 app = Flask(__name__)
@@ -15,8 +16,26 @@ DBSession = scoped_session(session_factory)
 session = DBSession()
 
 @app.route('/index/')
-def searchUPC():
+def displayEntryPage():
     return render_template('index.html')
+
+@app.route('/index/result/', methods=['GET', 'POST'])
+def searchUPC():
+    if request.method == 'POST':
+        upc = request.form['upc']
+
+        # Search in local database first
+        # If found redirect to product page for editing
+
+        # TODO
+
+        # Search in external databases
+        # If found redirect to create new item in local database page
+        # If not found redirect to create new item from scratch in local database page
+        productInfo = foodAPI.getProductByUPC(upc)
+        return jsonify(productInfo)
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
